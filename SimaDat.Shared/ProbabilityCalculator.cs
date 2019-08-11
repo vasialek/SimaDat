b2c19974501a6da7e4eb0bb4e107042c5c04ab90
Aleksej Vasinov <proglamer@gmail.com>
2019-08-11 16:13:56 +0300

    Added Probability calculator to ease calculations.

---
+++ SimaDat.Shared/ProbabilityCalculator.cs
@@ -0,0 +1,20 @@
+﻿using SimaDat.Models;
+using SimaDat.Models.Enums;
+
+namespace SimaDat.Shared
+{
+	public static class ProbabilityCalculator
+	{
+		public static float ProbabilityToKiss(int heroCharm, FriendshipLevels friendshipLevel)
+		{
+			// [0 - 100]
+			float charm = heroCharm * 100f / MySettings.MaxCharmForHero;
+			// [0 - 2]
+			float friendhsip = ((int)friendshipLevel - (int)FriendshipLevels.Familar);
+
+			// Max probability is 0.8 + 0.18 = 0.98
+			return charm * 0.008f + friendhsip * 0.09f;
+
+		}
+	}
+}
---
+++ SimaDat.UnitTests/ProbabilityCalculatorTest.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SimaDat.Shared;
+
+namespace SimaDat.UnitTests
+{
+	[TestClass]
+	public class ProbabilityCalculatorTest
+	{
+		[TestMethod]
+		public void ProbabilityToKiss()
+		{
+			// 0.8 * 100 / 200 + 0.09 * (3 - 2) = 0.49
+			float p = ProbabilityCalculator.ProbabilityToKiss(100, Models.Enums.FriendshipLevels.Friend);
+
+			p.Should().Be(0.49f);
+		}
+	}
+}
